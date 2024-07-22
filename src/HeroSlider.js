7769abb4fafa865fb750d144e1c7785c7d6e9e74
mealuc <aluc_78@hotmail.com>
2024-07-22 19:41:56 +0300

    heroslider component were improved in terms of work style

--- src/HeroSlider.js
+++ src/HeroSlider.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './HeroSlider.css';
 import image1 from './resources/heroSlider/1.jpg';
 import image2 from './resources/heroSlider/2.jpg';
@@ -7,34 +7,72 @@ import image3 from './resources/heroSlider/3.jpeg';
 function HeroSlider() {
     //Hero Slider 
     const [slideIndex, setSlideIndex] = useState(0);
-    let slideElementRef = useRef([]);
-    const slides = [
-        { slideNumber: "1", slideDescription: "Slide One", slideImage: image1 },
-        { slideNumber: "2", slideDescription: "Slide Two", slideImage: image2 },
-        { slideNumber: "3", slideDescription: "Slide Three", slideImage: image3 }
-    ]
+    const [focusIndex, setFocusIndex] = useState(0);
 
-    function changeSlides(index) {
-        setSlideIndex((slideIndex) => ( slideIndex + index + slides.length) % slides.length)
+    const slides = [
+        { slideNumber: 0, slideDescription: "Slide One", slideImage: image1 },
+        { slideNumber: 1, slideDescription: "Slide Two", slideImage: image2 },
+        { slideNumber: 2, slideDescription: "Slide Three", slideImage: image3 }
+    ]
+    const slideContextRef = useRef(null);
+    const slideElementRef = useRef([]);
+    const paginationRefs = useRef([]);
+
+    useEffect(() => {
+        changeSlide(slideIndex);
+        if (paginationRefs.current[slideIndex]) {
+            paginationRefs.current[slideIndex].focus();
+        }
+    }, [slideIndex])
+
+    function changeIndex(index) {
+        setSlideIndex(() => {
+            const newIndex = (index > slides.length - 1) ? 0 : (index < 0) ? slides.length - 1 : index;
+            changeSlide(newIndex);
+            return newIndex;
+        });
+    }
+
+    function changeFocus(index){
+        setFocusIndex(index);
+    }
+
+    function changeSlide(newIndex) {
+        slideContextRef.current.style.left = `-${newIndex * slideContextRef.current.clientWidth}px`;
     }
 
     return (
-        <div className="slideContainer">
-            <button className="arrows prev" onClick={() => changeSlides(-1)}>&#10094;</button>
-            <button className="arrows next" onClick={() => changeSlides(1)}>&#10095;</button>
-            {slides.map((slide, key) => {
-                return (
-                    <div key={key} ref={ref => slideElementRef.current[key] = ref}
-                        className={`slide ${key == slideIndex ? 'active' : ''}`}>
-                        <div className="slideNumber">{slide.slideNumber}</div>
-                        <div className="slideDescription">{slide.slideDescription}</div>
-                        <img src={slide.slideImage} alt={slide.slideDescription} />
-                    </div>
-                )
-            })}
-
+        <div className="slideContainer" >
+            <button className="arrows prev" onClick={() => changeIndex(slideIndex - 1)}>&#10094;</button>
+            <button className="arrows next" onClick={() => changeIndex(slideIndex + 1)}>&#10095;</button>
+            <ul className="paginationContainer">
+                {slides.map((element, index) => {
+                    return (
+                        <li key={index}>
+                            <input
+                                ref={ref => paginationRefs.current[index] = ref}
+                                className={`paginationInput ${index === focusIndex ? 'focused' : ''}`}
+                                type="button"
+                                role="tab"
+                                onClick={() => changeIndex(index)} 
+                                onFocus={() => changeFocus(index)}
+                                />
+                        </li>
+                    )
+                })}
+            </ul>
+            <div className="slideContext" ref={slideContextRef}>
+                {slides.map((slide, key) => {
+                    return (
+                        <div key={key} className="slide" ref={ref => slideElementRef.current[key] = ref}>
+                            <div className="slideNumber">{slide.slideNumber + 1}</div>
+                            <img src={slide.slideImage} alt={slide.slideDescription} />
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     )
 }
 
-export default HeroSlider = React.memo(HeroSlider);
+export default React.memo(HeroSlider);
